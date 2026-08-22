5da4c0a6fe297949b3571cf12a1587917fc033cb
VANSH <vishix07@gmail.com>
2026-08-22 22:00:58 +0530

    Using Fragment

---
+++ src/Fragments.jsx
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+
+function Fragments() {
+
+    return (
+        <Fragment>
+            <h1>Fragment in React js</h1>
+            <h1>Fragment in React js</h1>
+        </Fragment>
+        
+    )
+
+}
+
+export default Fragments;
