dfbba275ea83ab730bc65ca972aed6d9d5cf3f7d
VANSH <vishix07@gmail.com>
2026-08-25 00:31:26 +0530

    Context API

---
+++ src/CollageContext.jsx
@@ -0,0 +1,17 @@
+import ClassComponent from "./ClassComponent";
+
+ function CollageContext() {
+
+
+    return (
+
+    <div style={{backgroundColor:"green" , padding:"10px"}} >
+      <h1>Collage Component</h1>
+      <ClassComponent />
+    </div>
+  )
+
+  
+}
+
+export default CollageContext;
